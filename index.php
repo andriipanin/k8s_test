81846f48725786ae2e36845e4cee5f28e77adf00
Andrii.Panin <andy.panin@gmail.com>
2026-01-21 21:27:18 +0200

    Fixed errors

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 $ip_server = $_SERVER['SERVER_ADDR'];
-$your_ip   = $_SERVER['REMOTE_ADDR'];
 $user_agent= $_SERVER['HTTP_USER_AGENT'];
 
-echo "<h2>NIX onborading to Kubernetes</h2><br>";
+echo "<h2>NIX onboarding to Kubernetes</h2><br>";
 echo "Server IP Address is: $ip_server", "<br><p>";
-echo "Your IP is: $your_ip","<br><p>";
 echo "User agent: $user_agent","<br><p>";
+
 ?>
 
