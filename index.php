1fc109733e041b6dd753ad511080591de9685bae
Andrii.Panin <andy.panin@gmail.com>
2026-01-21 20:21:53 +0200

    fixed issue with IP

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $user_agent= $_SERVER['HTTP_USER_AGENT'];
 
 echo "<h2>NIX onborading to Kubernetes</h2><br>";
 echo "Server IP Address is: $ip_server", "<br><p>";
-echo "Your IP is: $you_ip","<br><p>";
+echo "Your IP is: $your_ip","<br><p>";
 echo "User agent: $user_agent","<br><p>";
 ?>
 
