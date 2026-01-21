fda2bf14639689137d1d38b5c20e9adba4344ad3
Andrii.Panin <andy.panin@gmail.com>
2026-01-21 21:36:53 +0200

    added picture

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ RUN yum -y update && \
     yum clean all
 
 COPY ./index.php /var/www/html/index.php
+COPY ./image_2026-01-21_21-23-28.png /var/www/html/image_2026-01-21_21-23-28.png
 
 
 
--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $user_agent= $_SERVER['HTTP_USER_AGENT'];
 echo "<h2>NIX onboarding to Kubernetes</h2><br>";
 echo "Server IP Address is: $ip_server", "<br><p>";
 echo "User agent: $user_agent","<br><p>";
+echo '<img src="image_2026-01-21_21-23-28.png" alt="Учи кубер">'
 
 ?>
 
